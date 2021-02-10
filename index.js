22d3cc45f59926bbe49e5dae1cfc634602f9e007
xtracynic <xtracynic@users.noreply.github.com>
2021-02-10 15:35:39 +0100

    auth flow

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/Users"); // This has to be above any services or you'll get an error since the services needs the models to exist.
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT || 5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
